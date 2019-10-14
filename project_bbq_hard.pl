d50ec2f638a591256d355ccb38b269f0fcb84f83
Roi <roiroi15@gmail.com>
2019-10-14 21:04:57 +0300

    replaces indexes row&column

--- project_bbq_hard.pl
+++ project_bbq_hard.pl
@@ -87,12 +87,12 @@ max(A, B, B) :-
 % Summary  - prints a the game board in the UI format
 % ---------------------------------------------------------------------------------- 
 print_board(Board) :-
-	print_all_board(Board, 1),!.
+	print_all_board(Board, 97),!.
 
 print_all_board(b(Size, Rows), LineIndex) :-
 	print_border_line_seperator(Size),nl,!,
 	print_board_rest(b(Size, Rows), LineIndex),
-	print_columns_index_marks.
+	print_columns_index_marks(Size),nl.
 
 print_board_rest(b(Size, []), _) :- 
 	print_border_line_seperator(Size),nl,!.
@@ -100,7 +100,7 @@ print_board_rest(b(Size, []), _) :-
 print_board_rest(b(Size, [Row|OtherRows]), LineIndex) :-
 	Size > 0,
 	write('|'),!,
-	print_row(Row), write('     <-- '), write(LineIndex),nl,
+	print_row(Row), write('     <-- '), name(LineIndexChar, [LineIndex]), write(LineIndexChar),nl,
 	LineIndex1 is LineIndex + 1,
 	(OtherRows = [] ; print_line_seperator(Size),nl),
 	print_board_rest(b(Size, OtherRows),LineIndex1).
@@ -175,15 +175,36 @@ print_border_line_seperator_rest(Size) :-
 
 % ---------------------------------------------------------------------------------- 
 % Predicate- print_columns_index_marks
-% Summary  - prints the columns indexes signs
+% Summary  - prints arrows and integer indexes at the bottom of the board 
 % ---------------------------------------------------------------------------------- 
-print_columns_index_marks :-
-     	write(' /\  /\  /\  /\  /\  /\ '),nl,
-	write(' |   |   |   |   |   | '),nl,
-	write(' a   b   c   d   e   f '),nl,!. 
+print_columns_index_marks(Size) :-
+	write(' '),print_n_times(' /\ ', Size), nl,
+	print_n_times('  | ',  Size) ,nl,
+	print_integer_indexes(1, Size), nl, !.
 
 
+print_integer_indexes(CurrentIndex, LastIndex) :- 
+	CurrentIndex =< LastIndex,
+	write('  '), write(CurrentIndex), write(' '),
+	CurrentIndex1 is CurrentIndex + 1,
+	print_integer_indexes(CurrentIndex1, LastIndex).
 
+print_integer_indexes(Index, Index) :- !.
+
+
+% ---------------------------------------------------------------------------------- 
+% Predicate- print_n_times
+% Summary  - print var given N times
+% ---------------------------------------------------------------------------------- 
+print_n_times(_, 0) :- !.
+
+print_n_times(ToPrint, N) :-
+	N > 0,
+	write(ToPrint),
+	N1 is N - 1,
+	print_n_times(ToPrint, N1).
+
+	
 
 % -----------------------------------------------------------------------------------------------------------------
 % 								 Board Manipulating Predicates  						 
@@ -312,16 +333,16 @@ place_in_board_index(Board, Sign, Index, NewBoard) :-
 	Board = b(Size, Rows),
 	(Sign = q ; Sign = b),
 	
-	Index = C/R,
+	Index = R/C,
 	(integer(R), atom(C),!,
 	name(C, [CAscii]),
-	ColIndex is CAscii - 96,
-	RowIndex is R
+	RowIndex is CAscii - 96,
+	ColIndex is R
 	;
 	integer(C), atom(R),!,
 	name(R, [RAscii]),
-	ColIndex is RAscii - 96,
-	RowIndex is C
+	RowIndex is RAscii - 96,
+	ColIndex is C
 	),
 
 	RowIndex > 0, RowIndex =< Size,
